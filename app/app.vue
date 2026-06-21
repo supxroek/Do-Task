a44f1035fd502dd2e772743e3aa4dc276c731a4f
Suparoek Manajit <oaffanmanajit@gmail.com>
2026-06-21 15:04:42 +0700

    feat: add new UI components and styles
    - Introduced Badge and Button components with variants using class-variance-authority.
    - Added utility functions for class name merging.
    - Created a new Tailwind CSS configuration file and imported necessary styles.
    - Updated package dependencies including Tailwind CSS and Lucide icons.
    - Added a components.json configuration for shadcn-vue integration.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold underline">Hello, Nuxt!</h1>
+
+    <Button class="mt-4" variant="default" size="default">
+      Click me
+    </Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
@@ -8,19 +9,24 @@ export default defineNuxtConfig({
     "nuxt-mongoose",
     "nuxt-lucide-icons",
     "dayjs-nuxt",
-    "@nuxtjs/tailwindcss",
     "@nuxtjs/google-fonts",
   ],
 
+  css: ["./app/assets/css/tailwind.css"],
   vite: {
+    plugins: [tailwindcss()],
     optimizeDeps: {
       include: [
         "@vue/devtools-core",
         "@vue/devtools-kit",
+        "class-variance-authority",
+        "clsx",
         "dayjs", // CJS
         "dayjs/plugin/relativeTime", // CJS
         "dayjs/plugin/updateLocale", // CJS
         "dayjs/plugin/utc", // CJS
+        "reka-ui",
+        "tailwind-merge",
       ],
     },
   },
